74d90418e6e02e54502763a347d5e45edd23b183
معلم بان <moalemban@gmail.com>
2025-09-08 20:46:35 +0000

    Try fixing this error: `Console Error: Error: Failed to read the 'cssRul

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,6 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased bg-secondary/30">
         {children}
